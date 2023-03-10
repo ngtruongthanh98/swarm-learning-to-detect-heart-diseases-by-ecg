cb93f2320feb0e78d2531f36720a3f6707438ffa
ngtruongthanh98 <ngtruongthanh98@gmail.com>
2023-03-10 22:07:21 +0700

    feat: Initialize Frontend portal

---
+++ swarm-learning-ecg-portal/pages/Home/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="color-text">Home page</div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
+
+<style lang="scss" scoped>
+.color-text {
+  color: red;
+}
+</style>
