ee7b637afd4631ef4cb228a587a1026a21b90855
ThanhNT26 <ngtruongthanh98@gmail.com>
2023-03-11 13:09:16 +0700

    feat: Create footer component

---
+++ ecg-portal/components/FooterComponent/index.vue
@@ -0,0 +1,54 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <footer class="footer">
+    <div class="container">
+      &copy; {{ currentYear }}
+      <span class="flex ml-2 mr-2"
+        >Created by
+        <a href="https://github.com/ngtruongthanh98" class="flex ml-2 mr-2"
+          ><svg
+            class="w-6 h-6 text-gray-600 hover:text-gray-800 button--github mr-1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            aria-hidden="true"
+            role="img"
+            width="32"
+            height="32"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 2.247a10 10 0 0 0-3.162 19.487c.5.088.687-.212.687-.475c0-.237-.012-1.025-.012-1.862c-2.513.462-3.163-.613-3.363-1.175a3.636 3.636 0 0 0-1.025-1.413c-.35-.187-.85-.65-.013-.662a2.001 2.001 0 0 1 1.538 1.025a2.137 2.137 0 0 0 2.912.825a2.104 2.104 0 0 1 .638-1.338c-2.225-.25-4.55-1.112-4.55-4.937a3.892 3.892 0 0 1 1.025-2.688a3.594 3.594 0 0 1 .1-2.65s.837-.262 2.75 1.025a9.427 9.427 0 0 1 5 0c1.912-1.3 2.75-1.025 2.75-1.025a3.593 3.593 0 0 1 .1 2.65a3.869 3.869 0 0 1 1.025 2.688c0 3.837-2.338 4.687-4.563 4.937a2.368 2.368 0 0 1 .675 1.85c0 1.338-.012 2.413-.012 2.75c0 .263.187.575.687.475A10.005 10.005 0 0 0 12 2.247z"
+              fill="currentColor"
+            />
+          </svg>
+          <span>ngtruongthanh98</span></a
+        ></span
+      >
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  background-color: #f8f8f8;
+  padding: 20px;
+  text-align: center;
+}
+
+.container {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+}
+</style>
