7a2808cea6564210a3d816a6b377d2cadab99859
ThanhNT26 <ngtruongthanh98@gmail.com>
2023-03-13 19:14:57 +0700

    test: Test ECG viewer UI

--- ecg-portal/components/hero/Hero.vue
+++ ecg-portal/components/hero/Hero.vue
@@ -2,10 +2,9 @@
   <div class="p-10 lg:p-36 hero-wrapper">
     <h1 class="text-white text-4xl mb-5">ECG Portal</h1>
     <h2 class="text-white text-2xl">
-      ECG Portal uses swarm learning to analyze and predict heart issues via ECG
-      data,
+      Using swarm learning to analyze and predict heart issues
       <br />
-      providing real-time evaluations with collaborating hospitals.
+      Providing real-time evaluations with collaborating hospitals
     </h2>
   </div>
 </template>
