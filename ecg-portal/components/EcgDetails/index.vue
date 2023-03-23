6e9fed4dbc4458f4dea4b7ac2f4623f3482ce847
ThanhNT26 <ngtruongthanh98@gmail.com>
2023-03-23 07:41:55 +0700

    refactor: Handle result container

--- ecg-portal/components/EcgDetails/index.vue
+++ ecg-portal/components/EcgDetails/index.vue
@@ -1,62 +1,6 @@
 <template>
   <div class="ecg-details-container">
-    <div class="result-ecg">
-      <div class="result-ecg__header">
-        <div class="header-icon">
-          <div class="content">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              width="16"
-              height="16"
-            >
-              <defs>
-                <linearGradient
-                  id="b"
-                  x1="-1483.396"
-                  x2="-1155.767"
-                  y1="1056.787"
-                  y2="1056.787"
-                  gradientUnits="userSpaceOnUse"
-                  xlink:href="#a"
-                />
-                <linearGradient id="a">
-                  <stop offset="0" stop-color="#fcd635" />
-                  <stop offset="1" stop-color="#f7a928" />
-                </linearGradient>
-              </defs>
-              <path
-                fill="url(#b)"
-                d="M-1220 1212.362c-11.656 8.326-86.446-44.452-100.77-44.568-14.324-.115-89.956 51.449-101.476 42.936-11.52-8.513 15.563-95.952 11.247-109.61-4.316-13.658-76.729-69.655-72.193-83.242 4.537-13.587 96.065-14.849 107.721-23.175 11.656-8.325 42.535-94.497 56.86-94.382 14.323.116 43.807 86.775 55.327 95.288 11.52 8.512 103.017 11.252 107.334 24.91 4.316 13.658-68.99 68.479-73.527 82.066-4.536 13.587 21.133 101.451 9.477 109.777z"
-                color="#000"
-                overflow="visible"
-                transform="matrix(.04574 0 0 .04561 68.85 -40.34)"
-                style="marker: none"
-              />
-            </svg>
-          </div>
-        </div>
-
-        <div class="title">
-          ECG Results Prediction
-          <span class="sub-title">{{ '(' + extendedText + ')' }}</span>
-        </div>
-      </div>
-
-      <div class="result-ecg__boby">
-        <div
-          v-for="(ele, index) in RESULT_DATA_MOCK"
-          class="result-ecg__item"
-          :key="index"
-        >
-          <div class="title">{{ ele.title }}</div>
-          <div class="value">
-            <span :class="ele.additionalClass">{{ ele.value }}</span>
-            {{ ele.unit }}
-          </div>
-        </div>
-      </div>
-    </div>
+    <results-prediction :extended-text="extendedText" />
 
     <div class="general-info">
       <div class="general-info__header">
@@ -127,9 +71,11 @@
 
 <script>
 import EcgViewer from '@/components/EcgViewer'
+import ResultsPrediction from '@/components/ResultsPrediction'
 
 export default {
   components: {
+    ResultsPrediction,
     EcgViewer,
   },
   props: {
---
+++ ecg-portal/components/ResultsPrediction/index.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="result-ecg">
+    <div class="result-ecg__header">
+      <div class="header-icon">
+        <div class="content">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="16"
+            height="16"
+          >
+            <defs>
+              <linearGradient
+                id="b"
+                x1="-1483.396"
+                x2="-1155.767"
+                y1="1056.787"
+                y2="1056.787"
+                gradientUnits="userSpaceOnUse"
+                xlink:href="#a"
+              />
+              <linearGradient id="a">
+                <stop offset="0" stop-color="#fcd635" />
+                <stop offset="1" stop-color="#f7a928" />
+              </linearGradient>
+            </defs>
+            <path
+              fill="url(#b)"
+              d="M-1220 1212.362c-11.656 8.326-86.446-44.452-100.77-44.568-14.324-.115-89.956 51.449-101.476 42.936-11.52-8.513 15.563-95.952 11.247-109.61-4.316-13.658-76.729-69.655-72.193-83.242 4.537-13.587 96.065-14.849 107.721-23.175 11.656-8.325 42.535-94.497 56.86-94.382 14.323.116 43.807 86.775 55.327 95.288 11.52 8.512 103.017 11.252 107.334 24.91 4.316 13.658-68.99 68.479-73.527 82.066-4.536 13.587 21.133 101.451 9.477 109.777z"
+              color="#000"
+              overflow="visible"
+              transform="matrix(.04574 0 0 .04561 68.85 -40.34)"
+              style="marker: none"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div class="title">
+        ECG Results Prediction
+        <span class="sub-title">{{ '(' + extendedText + ')' }}</span>
+      </div>
+    </div>
+
+    <div class="result-ecg__boby">
+      <div
+        v-for="(ele, index) in RESULT_DATA_MOCK"
+        class="result-ecg__item"
+        :key="index"
+      >
+        <div class="title">{{ ele.title }}</div>
+        <div class="value">
+          <span :class="ele.additionalClass">{{ ele.value }}</span>
+          {{ ele.unit }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      RESULT_DATA_MOCK: [
+        {
+          title: 'Normal ECG',
+          value: '70%',
+        },
+        {
+          title: 'Abnormal ECG',
+          value: '5%',
+        },
+        {
+          title: 'Borderline ECG',
+          value: '2%',
+        },
+        {
+          title: 'Otherwise normal ECG',
+          value: '23%',
+        },
+      ],
+    }
+  },
+  props: {
+    extendedText: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.result-ecg {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  padding: 16px;
+  width: 50%;
+  box-shadow: rgba(0, 0, 0, 0.08) 0px 4px 12px;
+  border-radius: 5px;
+
+  &:hover {
+    box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px,
+      rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+  }
+
+  &__header {
+    display: flex;
+    align-items: center;
+
+    .header-icon {
+      background-color: $error-color;
+      width: 48px;
+      height: 48px;
+      border-radius: 5px;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .content {
+        color: white;
+      }
+    }
+
+    .title {
+      margin-left: 16px;
+      font-size: 20px;
+      font-weight: 500;
+
+      .sub-title {
+        font-size: 14px;
+        font-style: italic;
+      }
+    }
+  }
+
+  &__boby {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 16px;
+  }
+
+  &__item {
+    display: flex;
+    flex-direction: column;
+
+    .title {
+      font-size: 14px;
+    }
+
+    .value {
+      font-size: 20px;
+      .bold {
+        font-weight: 500;
+      }
+    }
+  }
+}
+</style>
