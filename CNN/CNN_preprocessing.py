6709c6755c69e2debc61ad80b218a4d1265c17ba
nhan vu <vuthanhnhan1411@gmail.com>
2023-04-24 17:53:19 +0700

    feature/flask-cnn

--- CNN/CNN_preprocessing.py
+++ CNN/CNN_preprocessing.py
@@ -1,12 +1,7 @@
 import numpy as np
 import pandas as pd
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.layers import Reshape, Flatten, Input, Dense, Embedding, Conv2D, MaxPooling2D, Dropout, concatenate
-from tensorflow.keras import regularizers
-from tensorflow.keras.models import Model
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.optimizers import Adam
-from sklearn.metrics import confusion_matrix
+
 
 # Define constants
 NUM_SAMPLES = 18000
@@ -34,28 +29,20 @@ def preprocess_data_and_labels(selected_samples, labels):
     X = np.reshape(X, (X.shape[0], X.shape[2], X.shape[1], 1))
     return X, y
 
-# Load csv file with labels
-labels_df = pd.read_csv('.csv', sep=';')
-
-# Create a dictionary to map class labels to indices
-label_dict = {"Normal ECG": 0, "Abnormal ECG": 1, "Otherwise normal ECG": 2, "Borderline ECG": 3}
-
-# Create empty arrays to store selected samples and their labels
 selected_samples = []
 selected_labels = []
-pulse2pulse_150k_ground_truth
-# Loop over each class label
-for label in label_dict:
-    # Get indices of samples with current label
-    indices = labels_df.loc[labels_df['ecgcategory'] == label]['patid'].values
-    # Randomly select 18000 samples from the current label's indices
-    selected_indices = np.random.choice(indices, size=18000//len(label_dict), replace=False)
-    # Load and preprocess the selected samples
-    for patid in selected_indices:
-        filename = 'pulse2pulse_150k/from_006_chkp_2500_150k/{}.asc'.format(patid)
-        data = load_data(filename)
-        selected_samples.append(data)
-        selected_labels.append(label_dict[label])
+label_dict = {"Normal ECG": 0, "Abnormal ECG": 1, "Otherwise normal ECG": 2, "Borderline ECG": 3}
+
+# Load csv file with labels
+labels_df = pd.read_csv('pulse2pulse_150k_ground_truth.csv', sep=';')
+
+for i in range(30000, 60000):
+  filename = 'pulse2pulse_150k/from_006_chkp_2500_150k/{}.asc'.format(i)
+  data = load_data(filename)
+  selected_samples.append(data)
+  label = labels_df.loc[i, 'ecgcategory']
+  selected_labels.append(label_dict[label])
+
 
 # Convert selected samples and labels to numpy arrays
 selected_samples = np.array(selected_samples)
@@ -65,4 +52,5 @@ print("🚀 ~ file: CNN_preprocessing.py:67 ~ selected_labels:", selected_labels
 # Preprocess selected samples and labels
 X, y = preprocess_data_and_labels(selected_samples, selected_labels)
 print('X', X.shape)
-np.savez('preprocessed_data.npz', X=X, y=y)
+np.savez('backmai_preprocessed_data.npz', X=X, y=y)
+ 
--- CNN/CNN_prod.py
+++ CNN/CNN_prod.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 import numpy as np
-import io
+import json
 
-model = load_model('CNN_ECG.h5')
+model1 = load_model('CNN_ECG.h5')
+model2 = load_model("choray_CNN_ECG.h5")
+
 NUM_TIMESTEPS = 5000
 
 def load_data(filename):
@@ -22,20 +24,30 @@ def preprocess_data(data):
 
 data = load_data('110835.asc')
 input = preprocess_data(data)
-y_pred = model.predict(input)
+y_pred = model1.predict(input)
 label_dict = {0: "Normal ECG", 1: "Abnormal ECG", 2: "Otherwise normal ECG", 3: "Borderline ECG"}
 
 app = Flask(__name__)
 
-@app.route('/CNN', methods=['POST'])
-def predict_ecg():
+@app.route('/result/<int:hospital_id>', methods=['POST'])
+def predict_ecg(hospital_id):
+    if hospital_id == 1:
+        model = model1
+    elif hospital_id == 2:
+        model = model2
     file = request.files['file']
     raw_data = file.read()
     raw_data = np.fromstring(raw_data, sep='\n').reshape((5000, 8))
     data = preprocess_data(raw_data)
     y_pred = model.predict(data)
-    label = label_dict[np.argmax(y_pred, axis=1)[0]]
-    return jsonify({'label': label})
+    normalized_predict = [(val/sum(y_pred[0]))*100 for val in y_pred[0]]
+
+    # create a list of dictionaries with the label and percentage values
+    result = [{'title': label_dict[i], 'value': percent} for i, percent in enumerate(normalized_predict)]
+
+    # convert the list to a JSON object
+    json_result = json.dumps(result)
+    return json_result
 
 if __name__ == '__main__':
     app.run(debug=True)
