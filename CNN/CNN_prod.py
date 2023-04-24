97cf54245fdb2da9be3b3d9c374ebb14906a4535
nhan vu <vuthanhnhan1411@gmail.com>
2023-04-24 13:37:58 +0700

    feature: flask cnn

--- CNN/CNN_prod.py
+++ CNN/CNN_prod.py
@@ -1,5 +1,7 @@
+from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 import numpy as np
+import io
 
 model = load_model('CNN_ECG.h5')
 NUM_TIMESTEPS = 5000
@@ -9,9 +11,8 @@ def load_data(filename):
     data = np.genfromtxt(filename, delimiter=' ')
     return data
 
-def preprocess_data(filename):
+def preprocess_data(data):
     # Load data from file
-    data = load_data(filename)
     # Preprocess data
     X = np.zeros((1, NUM_TIMESTEPS, 8))
     X[0] = data
@@ -19,8 +20,22 @@ def preprocess_data(filename):
     X = np.reshape(X, (X.shape[0], X.shape[2], X.shape[1], 1))
     return X
 
-input = preprocess_data("pulse2pulse_150k/from_006_chkp_2500_150k/100000.asc")
+data = load_data('110835.asc')
+input = preprocess_data(data)
 y_pred = model.predict(input)
 label_dict = {0: "Normal ECG", 1: "Abnormal ECG", 2: "Otherwise normal ECG", 3: "Borderline ECG"}
 
-print(label_dict[np.argmax(y_pred, axis=1)[0]])
+app = Flask(__name__)
+
+@app.route('/CNN', methods=['POST'])
+def predict_ecg():
+    file = request.files['file']
+    raw_data = file.read()
+    raw_data = np.fromstring(raw_data, sep='\n').reshape((5000, 8))
+    data = preprocess_data(raw_data)
+    y_pred = model.predict(data)
+    label = label_dict[np.argmax(y_pred, axis=1)[0]]
+    return jsonify({'label': label})
+
+if __name__ == '__main__':
+    app.run(debug=True)
